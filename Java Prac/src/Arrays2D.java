828731627d29a2eaaeaf1392f64fd63b5bd70c15
zaid khan <120645965+zaidkhaaan@users.noreply.github.com>
2024-02-17 02:36:12 +0530

    2d-array

--- Java Prac/src/Arrays2D.java
+++ Java Prac/src/Arrays2D.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Arrays2D {
     public static void main(String[] args) {
-        
+
+        /*
+
+         QUES :- TO TAKE THE INPUT FOR 2D-ARRAY AND DISPLAY IT
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+
+
+        int [] [] arr = new int[n][m]; // syntax of 2-D array
+
+
+     // to take the input from user for 2-D array
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        // used to display the array
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j]+"\t");
+            }
+            System.out.println();
+        }
+
+
+         */
     }
 }
