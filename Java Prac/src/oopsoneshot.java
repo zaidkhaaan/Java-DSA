e67f9ba8f7d01c8cfc2c131e9c69ac67c8ed51f0
zaid khan <120645965+zaidkhaaan@users.noreply.github.com>
2024-03-18 04:08:45 +0530

    zx

---
+++ Java Prac/src/oopsoneshot.java
@@ -0,0 +1,22 @@
+public class oopsoneshot {
+
+
+    public static void main(String[] args) {
+        class Student {
+            String Name ;
+            int rno;
+            double percent;
+        }
+
+
+        Student x = new Student();
+
+        x.Name="Zaid";
+        x.rno = 76;
+        x.percent=92.5;
+        System.out.println(x.Name);
+
+
+
+    }
+}
