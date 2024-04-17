db3451152bc2266f14f0778ec89456b079fc430d
zaid khan <120645965+zaidkhaaan@users.noreply.github.com>
2024-04-18 00:19:02 +0530

    Update ALLINONE.java

--- Java Prac/src/PEPCODING_LEVEL_1/ALLINONE.java
+++ Java Prac/src/PEPCODING_LEVEL_1/ALLINONE.java
@@ -46,7 +46,6 @@ public class ALLINONE {
 //        System.out.println(a*(a+1)/2);
 
 
-
 //        int sum = 0;
 //        for (int i = a; i <=b ; i++) {
 //            sum += i;
@@ -57,9 +56,10 @@ public class ALLINONE {
 //        System.out.println(sum);
 
 
+  //      int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c =sc.nextInt();
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
 
 //        if (a > b)
 //        {
@@ -69,8 +69,114 @@ public class ALLINONE {
 //        }
 
 //
-        
+//        System.out.println(Math.max(a,b));
 
+//        if (a >= b && a >= c){
+//            System.out.println(a+" is greatest");
+//        }else if (b >= a && b >= c){
+//            System.out.println(b+ " is greatest");
+//        } else if (c >= a && c >=b) {
+//            System.out.println(c +" is greatest");
+//        }
+
+
+
+        /*
+        if ((a >= b) && (a >= c))  System.out.println (a + " is the greatest");
+
+            //checking if num2 is greatest
+        else if (b >= a && b >= c)  System.out.println (b + " is the greatest");
+
+            // num3 has to be greatest then if not above
+        else System.out.println (c + " is the greatest");
+
+         */
+
+//        int temp , result;
+//
+//        temp = a > b ? a:b ;
+//        result = temp > c ? temp:c ;
+//
+//        System.out.println(result);
+
+
+//
+
+        /*
+        if ((a % 4 == 0 && a % 100 != 0) || a % 400 == 0){
+    System.out.println("Leap Year");
+} else {
+    System.out.println("Not Leap Year");
+}
+
+         */
+
+
+//        if ((a % 4 == 0 && a % 100 != 0) || a % 400 == 0){
+//            System.out.println("Leap Year");
+//        }else{
+//            System.out.println("Not Leap Year");
+//        }
+
+
+
+        //  int sum;
+//
+
+
+//        Scanner sc = new Scanner (System.in);
+ //       String num = sc.nextLine();
+//        String num ="9876543219876543219876";
+
+     //   System.out.println("Sum of digits :"+getSum(num) );
+
+//
+
+
+//        String s = sc.nextLine();
+//       s= s.toLowerCase();
+//        String reversed = new StringBuilder(s).reverse().toString();
+//        boolean  c = reversed.equals(s);
+//        System.out.println(c);
+
+
+
+        int a = sc.nextInt();
+        int b = a ;
+        int c = a;
+        int count = 0;
+        while (a>0){
+            a = a/10;
+            count++;
+        }
+        int sum=0;
+        while (b>0){
+            int temp  = b%10;
+            sum += Math.pow(temp,count);
+            b=b/10;
+
+        }
+        if (sum == c){
+            System.out.println("Armstrong");
+        }else{
+            System.out.println("Not armstrong");
+        }
+    }
+
+
+    /*
+    static int getSum(String num)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < num.length(); i++) {
+            sum = sum+num.charAt(i) - 48;
+        }
+        return sum;
+    }
+
+
+     */
 
     }
 
@@ -91,4 +197,4 @@ public class ALLINONE {
 
 
 
-    }
+
