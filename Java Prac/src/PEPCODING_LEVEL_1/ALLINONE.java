c2227cd129d67e768b30935fad093ea4170612a5
zaid khan <120645965+zaidkhaaan@users.noreply.github.com>
2024-04-17 10:47:59 +0530

    Update ALLINONE.java

--- Java Prac/src/PEPCODING_LEVEL_1/ALLINONE.java
+++ Java Prac/src/PEPCODING_LEVEL_1/ALLINONE.java
@@ -1,4 +1,94 @@
 package PEPCODING_LEVEL_1;
 
+import java.util.Scanner;
+
 public class ALLINONE {
-}
+    public static void main(String[] args) {
+
+        /*
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if (a == 0){
+            System.out.println("ZERO");
+        } else {
+            String result = a > 0 ? "The number is positive ":"The number is negative";
+            System.out.println(result);
+        }
+
+         */
+
+        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+
+//        if (a % 2 == 0){
+//            System.out.println("Even");
+//        }else {
+//            System.out.println("Odd");
+//        }
+
+//        String result = a % 2 == 0 ? "Even" : "Odd";
+//        System.out.println(result);
+
+//        if (iseven(a)) {
+//            System.out.println("Even");
+//        } else {
+//            System.out.println("Odd");
+//        }
+
+
+//         int sum = 0;
+//        for (int i = 1; i <= a ; i++) {
+//             sum += i;
+//        }
+//        System.out.println(sum);
+//
+//        System.out.println(a*(a+1)/2);
+
+
+
+//        int sum = 0;
+//        for (int i = a; i <=b ; i++) {
+//            sum += i;
+//        }
+
+//        int sum = b*(b+1)/2 - a*(a+1)/2 + a;
+//
+//        System.out.println(sum);
+
+
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+//        if (a > b)
+//        {
+//            System.out.println(a+" is greater than b "+b);
+//        } else {
+//            System.out.println(b+" is greater than a "+a);
+//        }
+
+//
+        
+
+
+    }
+
+
+        /*
+        static boolean iseven (int number)
+
+
+            return (number & 1) == 0 ;
+            /*
+            For even numbers, the least significant bit is 0, so the result of the operation number & 1 will be 0.
+            For odd numbers, the least significant bit is 1, so the result will be 1.
+            Therefore, (number & 1) == 0 will return true for even numbers and false for odd numbers.
+            This makes the code more efficient as bitwise operations are faster
+             */
+
+
+
+
+
+    }
