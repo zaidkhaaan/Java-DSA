3f8fe7f541726e8acaa8c30120b4e7c993237243
zaid khan <120645965+zaidkhaaan@users.noreply.github.com>
2024-04-27 12:15:53 +0530

    Update Arrays2D.java

--- Java Prac/src/arrays/Arrays2D.java
+++ Java Prac/src/arrays/Arrays2D.java
@@ -49,39 +49,92 @@ public class Arrays2D {
         for (int i = 0; i < r1; i++) {
             for (int j = 0; j < c1; j++) {
                 a[i][j] = sc.nextInt();
+
+                /*
+                 (Row , Col)
+
+               Col(row, 0),  Col (row , 1) , Col (Row ,2)
+ Row(0,Col)         0 0  ,        0 1      ,     0 2
+ Row(1,Col)         1 0 ,         1 1      ,     1 2
+ Row(2,Col)         2 0 ,         2 1      ,     2 2
+
+                 */
             }
         }
 
-
-        System.out.println("Enter the number of rows & cols for 2nd matrix ");
-        int r2 = sc.nextInt();
-        int c2 = sc.nextInt();
-
-        int[][] a1 = new int[r2][c2];
-
-        System.out.println("Enter the matrix value");
-
-        for (int i = 0; i < r2; i++) {
-            for (int j = 0; j < c2; j++) {
-                a1[i][j] = sc.nextInt();
-            }
-        }
+//
+//        System.out.println("Enter the number of rows & cols for 2nd matrix ");
+//        int r2 = sc.nextInt();
+//        int c2 = sc.nextInt();
+//
+//        int[][] a1 = new int[r2][c2];
+//
+//        System.out.println("Enter the matrix value");
+//
+//        for (int i = 0; i < r2; i++) {
+//            for (int j = 0; j < c2; j++) {
+//                a1[i][j] = sc.nextInt();
+//            }
+//        }
 
 
         System.out.println("Matrix 1");
         printMatrix(a);
-        System.out.println("Matrix 2");
-        printMatrix(a1);
+//        System.out.println("Matrix 2");
+//        printMatrix(a1);
 
 
 
 
-        matMUL(a,r1,c1,a1,r2,c2);
+       // matMUL(a,r1,c1,a1,r2,c2);
+
+
+        System.out.println("Transpose Matrix");
+
+        //Transpose(a,r1,c1);
+        TransinPlace(a,r1,c1);
+
+
+
+    }
+
+
+    static void Transpose ( int [] [] a , int r1 , int c1 ) {
+
+
+        // Transpose of a matrix where a row is converted into col
+
+//        if (c1 != r1){
+//            System.out.println("Wrong Matrix Input");
+//        }
+
+        int arr [] [] = new int[c1][r1];
+        for (int i = 0; i < c1; i++) {
+            for (int j = 0; j < r1; j++) {
+                arr[i][j] = a[j][i];
+            }
+        }
+        printMatrix(arr);
+
+
+    }
+
+    static void TransinPlace (int [] [] matrix , int r , int c){
+
+        for (int i = 0; i < c; i++) {
+            for (int j = i; j < r; j++) {
+                int temp = matrix[i][j];
+                matrix [i][j] = matrix[j][i];
+                matrix [j][i] =temp;
+            }
+        }
+        printMatrix(matrix);
     }
 
 
 
 
+    /*
     static   void matMUL(int [] [] a , int r1, int c1 , int [] [] a1 , int r2, int c2){
         if (c1 != r2){
             System.out.println("Wrong Matrix input please make the col of first matrix equal to row of second matrix");
@@ -101,6 +154,8 @@ public class Arrays2D {
         printMatrix(arr);
 
     }
+
+     */
 //        add(a,r1,c1 ,a1,r2,c2);
 //
 //
