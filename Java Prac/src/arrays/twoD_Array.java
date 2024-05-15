696d0e530039434e3292c687a6952cf191a49c7e
zaid khan <120645965+zaidkhaaan@users.noreply.github.com>
2024-05-16 02:16:37 +0530

    xc

--- Java Prac/src/arrays/twoD_Array.java
+++ Java Prac/src/arrays/twoD_Array.java
@@ -1,4 +1,6 @@
 package arrays;
 
 public class twoD_Array {
+
+
 }
--- Java Prac/src/twoDarray/Arrays2D.java
+++ Java Prac/src/twoDarray/Arrays2D.java
@@ -156,43 +156,41 @@ public class Arrays2D {
 //        rotate(a,r);
 
 
-
-
-
-
-
-
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-int ans [][] = pascal(n);
-        printMatrix(ans);
-
-
-
-
-
+//
+//
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//int ans [][] = pascal(n);
+//        printMatrix(ans);
 
     }
-    static int  [] [] pascal ( int n ){
-        int [] [] ans = new int[n][];
+    
 
 
-        for (int i = 0; i < n; i++) {
 
-            // dynamically assign the colmun which jagged
-            ans[i] = new int [i+1];
-            // 1st and last element of every row is 1
-            ans[i][0] = ans [i][i] = 1;
 
-            for (int j = 1; j < i ; j++) {
-                ans[i][j]=ans[i-1][j]+ans[i-1][j-1];
 
-            }
-
-        }
-        return ans;
-
-    }
+//    }
+//    static int  [] [] pascal ( int n ){
+//        int [] [] ans = new int[n][];
+//
+//
+//        for (int i = 0; i < n; i++) {
+//
+//            // dynamically assign the colmun which jagged
+//            ans[i] = new int [i+1];
+//            // 1st and last element of every row is 1
+//            ans[i][0] = ans [i][i] = 1;
+//
+//            for (int j = 1; j < i ; j++) {
+//                ans[i][j]=ans[i-1][j]+ans[i-1][j-1];
+//
+//            }
+//
+//        }
+//        return ans;
+//
+//    }
 
 //    static void transpose(int arr[][], int r, int c) {
 //        if (r != c) {
