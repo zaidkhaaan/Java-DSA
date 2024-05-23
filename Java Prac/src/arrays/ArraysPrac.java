5ddd450a43a9e3dc0c6f8d92cbbbabd8b71ac208
zaid khan <120645965+zaidkhaaan@users.noreply.github.com>
2024-05-24 03:19:12 +0530

    Update ArraysPrac.java

--- Java Prac/src/arrays/ArraysPrac.java
+++ Java Prac/src/arrays/ArraysPrac.java
@@ -2315,7 +2315,8 @@ public class Main {
 
 
 
-        // Union of two array
+        // concatation of two arrays
+
 
 //        int arr [] = {1,2,3};
 //        int arr1[] = {4,5,6};
@@ -2334,32 +2335,134 @@ public class Main {
 //
         // intersection of array
 
-        int arr[] = {1,2,3};
-        int arr2[] = {4,1,0};
+//        int arr[] = {1,2,3};
+//        int arr2[] = {4,1,0};
+//
+//
+//        int sum = 0;
+//        for ( int i = 0; i <arr.length ; i++){
+//            for ( int j = 0 ; j < arr2.length;j++){
+//                if (arr[i]==arr2[j]){
+//                    sum++;
+//                }
+//            }
+//        }
+//
+//
+//        int int3[] = new int[sum];
+//        for ( int i = 0; i <arr.length ; i++){
+//            for ( int j = 0 ; j < arr2.length;j++){
+//                if (arr[i]==arr2[j]){
+//                   int3[i]=arr[i];
+//                }
+//            }
+//        }
+//        printArray(int3);
 
 
-        int sum = 0;
-        for ( int i = 0; i <arr.length ; i++){
-            for ( int j = 0 ; j < arr2.length;j++){
-                if (arr[i]==arr2[j]){
-                    sum++;
-                }
-            }
-        }
+
+        // program for 2nd largest
+
+//        int arr[] = {13,34,2,34,33,1};
+//        int max = arr[0];
+//        int max2= 0;
+//        for ( int i = 0 ; i <arr.length; i++) {
+//
+//            if (arr[i]>max) {
+//                max2=max;
+//                max = arr[i];
+//            }else if ( arr[i]!=max && arr[i]>max2){
+//                max2=arr[i];
+//            }
+//        }
+//
+//        System.out.println(max);
+//        System.out.println(max2);
 
 
-        int int3[] = new int[sum];
-        for ( int i = 0; i <arr.length ; i++){
-            for ( int j = 0 ; j < arr2.length;j++){
-                if (arr[i]==arr2[j]){
-                   int3[i]=arr[i];
-                }
-            }
-        }
-        printArray(int3);
+        // to find out 2 smallest element
+
+//        int arr[] = {3,34,2,34,33,1};
+//
+//        int min = arr[0];
+//        int min2 = 0;
+//        for ( int i = 0 ; i < arr.length; i++){
+//
+//            if (arr[i] < min){
+//                min2=min;
+//               min = arr[i];
+//            } else if (arr[i] != min && arr[i] < min2) {
+//                min2=arr[i];
+//            }
+//        }
+//        System.out.println(min2);
+//        System.out.println(min);
 
 
+        int arr[] = {10,5,10,15,10,5};
+
+
+
+        /*
+
+
+
+import java.util.*;
+
+class TUF{
+static ArrayList<Integer> FindUnion(int arr1[], int arr2[], int n, int m) {
+   int i = 0, j = 0; // pointers
+  ArrayList<Integer > Union=new ArrayList<>(); // Uninon vector
+  while (i < n && j < m) {
+    if (arr1[i] <= arr2[j]) // Case 1 and 2
+    {
+      if (Union.size() == 0 || Union.get(Union.size()-1) != arr1[i])
+        Union.add(arr1[i]);
+      i++;
+    } else // case 3
+    {
+      if (Union.size() == 0 || Union.get(Union.size()-1) != arr2[j])
+        Union.add(arr2[j]);
+      j++;
     }
+  }
+  while (i < n) // IF any element left in arr1
+  {
+    if (Union.get(Union.size()-1) != arr1[i])
+      Union.add(arr1[i]);
+    i++;
+  }
+  while (j < m) // If any elements left in arr2
+  {
+    if (Union.get(Union.size()-1) != arr2[j])
+      Union.add(arr2[j]);
+    j++;
+  }
+  return Union;
+}
+public static void main(String args[]) {
+  int n = 10, m = 7;
+  int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int arr2[] = {2, 3, 4, 4, 5, 11, 12};
+  ArrayList<Integer> Union = FindUnion(arr1, arr2, n, m);
+  System.out.println("Union of arr1 and arr2 is ");
+  for (int val: Union)
+    System.out.print(val+" ");
+}
+}
+
+
+
+         */
+
+
+        }
+
+
+
+
+
+
 //    static int [] sortsquares (int arr[]){
 //        int n = arr.length;
 //        int left = 0 , right= arr.length-1;
